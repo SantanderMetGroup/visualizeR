f1f15ec9d90304eaeee6ed1b4693b5699a978ae7
Your Name <maibide@gmail.com>
2017-11-17 15:47:58 +0100

    building tercileMap

---
+++ R/tercileMap.R
@@ -0,0 +1,16 @@
+hindcast <- subsetGrid(CFS_Iberia_tas, years = 1983:2001)
+forecast <- subsetGrid(CFS_Iberia_tas, years = 2002)
+library(downscaleR)
+tercileMap <- function(hindcast, forecast){
+    hindyear <- aggregateGrid(hindcast, aggr.y = list(FUN = mean, na.rm = TRUE))
+    foreyear <- aggregateGrid(forecast, aggr.y = list(FUN = mean, na.rm = TRUE))
+    
+    hind <- redim(downscaleR:::flatMemberDim(hindyear), drop = TRUE)
+    a <- apply(hind$Data, MARGIN = c(2,3), FUN = quantile, probs = c(1/3, 2/3))
+    nmem <- getShape(forecast)["member"]
+    for (i in 1:nmem) {
+      formem <- subsetGrid(foreyear, members = i)$data
+      
+    
+    }
+}
