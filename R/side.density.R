c295efe73095bd8a6315348a4a8a66b4f7a043e3
mdfrias <friasmd@unican.es>
2018-10-16 19:00:06 +0200

    Remove export in side.density.R

--- R/side.density.R
+++ R/side.density.R
@@ -17,7 +17,6 @@
 #' @details The function is intended for internal use only. The function adds the PDF to the cascade plot.
 #'  @author J. Fernandez and M.D. Frias 
 #'  @keywords internal
-#'  @export
 
 side.density <- function(x, horiz=TRUE, col="black", scale=0.1, alpha=0.3, 
   shades = FALSE, prob=c(0.25,0.5,0.75), density.offset=0, density.reverse=FALSE)
